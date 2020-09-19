909d74dadf24e9c17fb0d688759666afe4db19dc
Jayman Matthews <Jaymanmatthews69@gmail.com>
2020-09-20 00:26:37 +0800

    v0.001 - changelog ingame.

--- saveloadreset.js
+++ saveloadreset.js
@@ -34,13 +34,12 @@ window.onload = function() {
     document.getElementById("currency").innerHTML = numberFormatting(gameData.currency, 1)
     document.getElementById("basicupgrade1increase").innerHTML = numberFormatting(basicUpgrades.upgrade1.increaseArray[basicUpgrades.upgrade1.count], 1)
     if (basicUpgrades.upgrade1.level == basicUpgrades.upgrade1.maxLevel) {
-        document.getElementById("basicupgrade1cost").innerHTML = "Max Level Reached!"
-        document.getElementById("currentlevel").innerHTML = "Max Level Reached!"
-        document.getElementById("nextupgradeincrease1").innerHTML = "Max Level Reached!"
+        document.getElementById("basicupgrade1cost").innerHTML = "MAX"
+        document.getElementById("nextupgradeincrease1").innerHTML = "MAX"
     }
     else {
         document.getElementById("basicupgrade1cost").innerHTML = "Cost: " + numberFormatting(basicUpgrades.upgrade1.costArray[basicUpgrades.upgrade1.count], 1)
         document.getElementById("currentlevel").innerHTML = "Level " + numberFormatting(basicUpgrades.upgrade1.level, 0) + "/" + numberFormatting(basicUpgrades.upgrade1.maxLevel, 0)
-        document.getElementById("nextupgradeincrease1").innerHTML = "(Upgrade to increase currency by " + numberFormatting(basicUpgrades.upgrade1.increaseArray[basicUpgrades.upgrade1.count + 1], 1) + " per second)"
+        document.getElementById("nextupgradeincrease1").innerHTML = numberFormatting(basicUpgrades.upgrade1.increaseArray[basicUpgrades.upgrade1.count + 1], 1)
     }
 }
